0deb80b3eb9c65d8091dc39c79fd0988921ee408
Marcelo Vlaicevich <mvlaicevich@gmail.com>
2021-11-14 17:50:56 -0300

    initial commit

---
+++ src/main/java/com/uade/tp/bd2/Bd2Application.java
@@ -0,0 +1,13 @@
+package com.uade.tp.bd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uade/tp/bd2/Bd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.tp.bd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
